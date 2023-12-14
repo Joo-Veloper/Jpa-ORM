c92e57d303c69f35e131ac9e602df24831458e66
Joo-Veloper <psjoo3515@gmail.com>
2023-12-14 19:22:59 +0900

    refactoring

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,17 +12,18 @@ public class JpaMain {
                                                     // DB 커넥션을 얻어서 쿼리를 날리고 종료되는 그런 한 일관적인 단위를 할 때마다 이 엔티티 메니저라는 애를 꼭 만들어주어야함!!
         EntityTransaction tx = em.getTransaction();
         tx.begin();
+        try{
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB"); //JPA에서는 트랜젝션이라는 단위가 매우 중요함 모든 데이터를 변경하는 모든 작업은 JPA에서 꼭 transaction 안에서 작업을 해야 한다!!
 
-        Member member = new Member();
-        member.setId(2L);
-        member.setName("HelloB"); //JPA에서는 트랜젝션이라는 단위가 매우 중요함 모든 데이터를 변경하는 모든 작업은 JPA에서 꼭 transaction 안에서 작업을 해야 한다!!
-
-        em.persist(member); // 멤버 저장
-
-        tx.commit();
-        // code
-        em.close();
-
+            em.persist(member); // 멤버 저장
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
         emf.close();
     }
 }
