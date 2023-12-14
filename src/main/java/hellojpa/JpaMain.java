22147a24b9bacecf7c7237143e527d8972aa3d5b
Joo-Veloper <psjoo3515@gmail.com>
2023-12-14 19:53:07 +0900

    JPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -13,11 +14,15 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
+//            Member findMember = em.find(Member.class, 1L);
+            // JPQL
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList(); // JPA는 테이블 대상으로 절때 코드 짜지 않는다.!
+            for(Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
             tx.commit();
-
         } catch (Exception e) {
             tx.rollback();
         } finally {
@@ -26,7 +31,7 @@ public class JpaMain {
         emf.close();
     }
 }
-// 중의
+// 주의
 // 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유!!
 // 엔티티 매니저는 쓰레드간에 공유하면 안됨!!*사용하고 버려야함
 // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행 !!
