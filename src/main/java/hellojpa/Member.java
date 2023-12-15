6ef4f5d24cf439e0a822f38c43920bf76b9bb48d
Joo-Veloper <psjoo3515@gmail.com>
2023-12-15 16:33:08 +0900

    Table

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,8 +2,10 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Entity //jpa가 관리하는 객체!
+@Table(name = "MBR") // 예를들어 테이블을 다른 이름을 맵핑하고 싶어 dba가 멤버라고 쓰면 안되요! 에를 들어 mbr이라고 쓰야하 해요 라고 하면 @Table(name = "MBR")이라고 쓰면 됨
 //@Table(name = "USER") //멤버 이름이 다를때 : 데이터 베이스에 멤버가 아니라 유저 라고
                       // 되어있다면 @Table(name = "USER") 해주면 쿼리가 나갈때 user 테이블에 인서트 하고 나간다.!
 public class Member {
