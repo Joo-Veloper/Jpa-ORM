0a05331a7d3d5db163b745e32d4d21c343794453
Joo-Veloper <psjoo3515@gmail.com>
2023-12-20 11:45:01 +0900

    N:1

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -12,17 +12,19 @@ public class Team {
     private Long id;
     private String name;
 
+    //    @OneToMany(mappedBy = "team")
+//    //mappedBy (가짜 매핑)
+//    // 객체와 테이블간에 연관관계를 맺는 차이를 이해해야 한다!
+//    // 객체의 양방향 관계는 사실 양방향 관계가 아닌 서로 다른 다른 단방향 관계 2개 이다.
+//
+//    private List<Member> members = new ArrayList<>();
+//
+//    public void addMember(Member member) {
+//        member.setTeam(this);
+//        members.add(member);
+//    }
     @OneToMany(mappedBy = "team")
-    //mappedBy (가짜 매핑)
-    // 객체와 테이블간에 연관관계를 맺는 차이를 이해해야 한다!
-    // 객체의 양방향 관계는 사실 양방향 관계가 아닌 서로 다른 다른 단방향 관계 2개 이다.
-
     private List<Member> members = new ArrayList<>();
-
-    public void addMember(Member member) {
-        member.setTeam(this);
-        members.add(member);
-    }
 
     public Long getId() {
         return id;
@@ -40,20 +42,20 @@ public class Team {
         this.name = name;
     }
 
-    public List<Member> getMembers() {
-        return members;
-    }
-
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
-
-    @Override
-    public String toString() {
-        return "Team{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", members=" + members +
-                '}';
-    }
+//    public List<Member> getMembers() {
+//        return members;
+//    }
+//
+//    public void setMembers(List<Member> members) {
+//        this.members = members;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Team{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", members=" + members +
+//                '}';
+//    }
 }
