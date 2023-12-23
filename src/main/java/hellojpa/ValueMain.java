19c13a7052e25adbe696fa60339622b84ee43ef0
Joo-Veloper <psjoo3515@gmail.com>
2023-12-23 20:19:48 +0900

    equals

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -4,14 +4,13 @@ public class ValueMain {
     // 자바의 기본타입은 절대 공유 X
 
     public static void main(String[] args) {
-        Integer a = Integer.valueOf(10);
-        Integer b = a;
+        int a = 10;
+        int b = 10;
+        System.out.println(" a == b : " + (a == b));
 
-
-//        int a = 10;
-//        int b = a;
-//        a = 20;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+        System.out.println("address1 == address2 : " + (address1 == address2));
+        System.out.println("address1 equals address2 : " + (address1.equals(address2)));
     }
 }
